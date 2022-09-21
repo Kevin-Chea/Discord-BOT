3830a16d6cb473d0d5e0415094f0d2b61e1375e6
Kevin Chea <kevin.chea@epita.fr>
2022-09-21 20:32:30 +0200

    [DEBUG][Feature] on_message was preventing command to work now corrected

--- src/main.py
+++ src/main.py
@@ -26,19 +26,27 @@ async def on_message(message):
         returnMessage = "Salut tout seul"
         await channel.send(returnMessage)
         await channel.send(message.author.mention)
+    await bot.process_commands(message)
 
 @bot.command()
 async def pong(ctx):
     await ctx.send('pong')
 
+# Send back the username
 @bot.command(name="name")
 async def name(ctx):
     await ctx.send(ctx.author)
 
+# Send back a random number between 1 & 6
 @bot.command(name="d6")
 async def d6(ctx):
     n = randrange(1, 7, 1)
     await ctx.send(n)
+
+@bot.command(name="admin")
+async def admin(ctx):
+    t = discord.utils.get(ctx.guild.roles.name, name="admin")
+    await ctx.send(t)
 
 token = "MTAyMjE5Mjk5MDkwNDY0NzY5MQ.GrhVxn.52M0h6YOiJasJVC3Dpuv2-7cLkJMqySCS_k-jw"
 bot.run(token)  # Starts the bot
